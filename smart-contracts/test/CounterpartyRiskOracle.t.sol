28bdb50717b0415eba776616ce9ec1175b3d2785
Anibal Catalan <anibal@notabene.id>
2022-10-08 02:20:05 -0500

    chore: CRA minimal test

---
+++ smart-contracts/test/CounterpartyRiskOracle.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "./SigUtils.sol";
+import "@src/ICounterpartyRiskOracle.sol";
+import "@src/CounterpartyRiskOracle.sol";
+
+contract CounterpartyRiskOracleTest is Test {
+    SigUtils internal sigUtils;
+    ICounterpartyRiskOracle internal cra;
+
+    uint256 internal signerPrivateKey;
+    uint256 internal originatorPrivateKey;
+    uint256 internal beneficiaryPrivateKey;
+
+    address internal signer;
+    address internal originator;
+    address internal beneficiary;
+
+    function setUp() public {
+        signerPrivateKey = 0xA11CF;
+        signer = vm.addr(signerPrivateKey);
+
+        cra = new CounterpartyRiskOracle(signer);
+        sigUtils = new SigUtils(cra.getDomainHash());
+
+        originatorPrivateKey = 0xF13AB;
+        beneficiaryPrivateKey = 0xBF03B;
+
+        originator = vm.addr(originatorPrivateKey);
+        beneficiary = vm.addr(beneficiaryPrivateKey);
+    }
+
+    function testContructor() public {
+        assertEq(cra.getSigner(), signer);
+    }
+
+    function testverifyCounterpartyRiskTo() public {
+        
+       CounterpartyRiskOracle.CRA memory craMsg = ICounterpartyRiskOracle.CRA({
+            VASPAddress: address(this),
+            originator: originator,
+            beneficiary: address(this),
+            symbol: "WETH",
+            amount: 2,
+            expireAt: 1 days
+        });
+
+        SigUtils.CRA memory counterpartyRisk = SigUtils.CRA({
+            VASPAddress: address(this),
+            originator: originator,
+            beneficiary: address(this),
+            symbol: "WETH",
+            amount: 2,
+            expireAt: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(counterpartyRisk);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivateKey, digest);
+        bytes memory sig = abi.encodePacked(r, s, v);
+        bytes32 hashedMsg = sigUtils.getStructHash(counterpartyRisk);
+
+        bytes memory signature = cra.hashSignature(hashedMsg);
+
+        assertEq(signature.length, 0);
+
+        cra.verifyCounterpartyRisk(craMsg, sig);
+        
+        signature = cra.hashSignature(hashedMsg);
+
+        assertEq(signature, sig);
+    }
+
+
+
+    function testSetSigner() public {
+        address newSigner = address(0x1);
+        cra.setSigner(newSigner);
+        assertEq(cra.getSigner(), newSigner);
+    }
+}
---
+++ smart-contracts/test/SigUtils.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SigUtils {
+    bytes32 internal DOMAIN_SEPARATOR;
+
+    constructor(bytes32 _DOMAIN_SEPARATOR) {
+        DOMAIN_SEPARATOR = _DOMAIN_SEPARATOR;
+    }
+
+    // keccak256("Counter Party Risk Attestation(address VASPAddress, address originator, address beneficiary, string symbol, uint256 amount, uint256 expireAt)");
+    bytes32 public constant CRA_TYPEHASH =
+        0x70fa7e976ae7888309b0257837ad3f859742f368c48b27f3117be000a91c5497;
+
+    struct CRA {
+        address VASPAddress;
+        address originator;
+        address beneficiary;
+        string symbol;
+        uint256 amount;
+        uint256 expireAt;
+    }
+
+    // computes the hash of a counter party risk oracle
+    function getStructHash(CRA memory _msg)
+        public
+        view
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encode(
+                    CRA_TYPEHASH,
+                    _msg.VASPAddress,
+                    _msg.originator,
+                    _msg.beneficiary,
+                    _msg.symbol,
+                    _msg.amount,
+                    _msg.expireAt
+                )
+            );
+    }
+
+    // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
+    function getTypedDataHash(CRA memory _msg)
+        public
+        view
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR,
+                    getStructHash(_msg)
+                )
+            );
+    }
+}
